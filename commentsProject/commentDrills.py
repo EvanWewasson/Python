0c9843a63e0c5178c626920d6c88501e35b242bb
EvanWewasson <97648951+EvanWewasson@users.noreply.github.com>
2022-01-13 00:29:50 -0600

    1/12/22

---
+++ commentsProject/commentDrills.py
@@ -0,0 +1,43 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#I like books
+
+
+#2) Make a multi-line comment.
+
+"""
+I like
+books
+"""
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+x = 21 # x will equal 21
+
+#4) Make a single line comment.
+
+#I like anime
+
+#5) Make a multi-line comment.
+
+"""
+I like
+anime
+"""
