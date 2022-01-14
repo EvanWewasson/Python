2ac0954ab0ac19f54dfa3ab0ad5f05d970d21261
EvanWewasson <97648951+EvanWewasson@users.noreply.github.com>
2022-01-13 20:32:40 -0600

    1/13/22

---
+++ survey/survey.py
@@ -0,0 +1,39 @@
+'''
+Created on Jan 13, 2022
+
+The Objective is to make a program that can take survey information
+from a user. The tasks to complete are:
+Ask the user five different questions and get their answers
+The questions are:
+What is your name?
+What is your favorite color?
+What city did you grow up in?
+What is your best friend’s name?
+What color hair do you have?
+Store the user’s answers in variables
+Print the user’s answers.
+
+@author: Evan Wewasson
+'''
+
+'''
+userResponse = input(text_to_user)
+'''
+
+name = input("What is your name?")
+print(name)
+
+
+color = input("What is your favorite color?")
+print(color)
+
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friends name?")
+print(friend)
+
+
+hair = input("What color hair do you have?")
+print(hair)
