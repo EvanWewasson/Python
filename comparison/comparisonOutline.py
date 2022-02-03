8d71e9db073a3059330fdf4aebce0a2cd3a4ddef
EvanWewasson <97648951+EvanWewasson@users.noreply.github.com>
2022-02-02 23:43:16 -0600

    2/2/22

---
+++ comparison/comparisonOutline.py
@@ -0,0 +1,55 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+mat = 6
+tam = 6
+math = (tam == mat)
+print(math)
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+x = 3
+y = 3
+z = (x == y)
+print(z)
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+a = 5
+b = 4
+c = (a > b)
+print(c)
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+e = 7
+f = 9
+g = (e < f)
+print(g)
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+h = 4
+i = 4
+j = (h >= i)
+print(j)
+
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+k = 5
+l = 6
+m = (k <= l)
+print(m)
+
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+take = 6
+over = 4
+takeover = (take < over)
+print(takover)
+
