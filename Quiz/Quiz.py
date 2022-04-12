296962f2c0b583949e3ed07034120d64c18573a2
EvanWewasson <97648951+EvanWewasson@users.noreply.github.com>
2022-04-11 20:58:07 -0500

    4/11

--- Quiz/Quiz.py
+++ Quiz/Quiz.py
@@ -19,38 +19,49 @@ and at the end give them a score out of 4. And give them a percentage.
 
 @author: Evan Wewasson
 '''
-from pickle import TRUE
+
 '''
 userResponse = input(text_to_user)
 '''
-#Make a variable called score to keep track of the correct answers. And set
-#it to 0.
+
 score = 0
-#Ask the user question one. And store the users answer.
-one = input("1) What is the powerhouse of the cell?")
-#Check if the answer is correct, if it is add one to score and print out
-#"Correct"
-#Else, print out "Incorrect, the correct answer is A."
-A = True 
-if 
-#Ask the user question two. And store the users answer.
 
-#Check if the answer is correct, if it is add one to score and print out
-#"Correct"
-#Else, print out "Incorrect, the correct answer is C."
+print("1) What is the powerhouse of the cell?")
+one = input("Enter a letter: A) mitochondria B) nucleus C) ribosome")
 
-#Ask the user question three. And store the users answer.
+if (one.upper() =="A"):
+    score= score + 1
+    print("Correct")
+else:
+    print("Incorrect, the correct answer is A.")
 
-#Check if the answer is correct, if it is add one to score and print out
-#"Correct"
-#Else, print out "Incorrect, the correct answer is A."
+print("2) How many states comprise the United States?")
+two = input("Enter a letter: A) 13 B) 45 C) 50")
 
-#Ask the user question four. And store the users answer.
+if (two.upper() =="C"):
+    score = score + 1
+    print("Correct")
+else:
+    print("Incorrect, the correct answer is C.")
 
-#Check if the answer is correct, if it is add one to score and print out
-#"Correct"
-#Else, print out "Incorrect, the correct answer is C."
+print("3) In y = mx + b, what does m stand for?")
+three = input("Enter a letter: A) slope B) output C) I don't understand math")
 
-#Calculate the percentage the user got. And store it in a variable called p
+if (three.upper() =="A"):
+    score = score + 1
+    print("Correct")
+else:
+    print("Incorrect, the correct answer is A.")
 
-#Print out the users score: "You got a [score]/4. Or a [p]%."
+print("4) In English, a person, place or thing is called:")
+four = input("Enter a letter: A) verb B) adjective C) noun")
+
+if (four.upper() =="C"):
+    score = score + 1
+    print("Correct")
+else:
+    print("Incorrect, the answer is C.")
+
+p = (score / 4) * 100
+
+print("You got a " + str(score) + "/4. Or a " + str(p) + "%.")
